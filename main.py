c0bfe9e1c3ff42251405627e34913eea32b33fc5
GreatKwaksha <eshelshugol@gmail.com>
2026-04-21 21:18:29 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,22 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
+import requests
+import json
+from twilio.rest import Client
 
+api_key="52e3a2c07857777e728d6f680bcfe84c"
+token="cb0a4ea40b55bcae0db06005093894bf"
+sid="ACd43cadc60ae83267188af6210d036713"
+my_lat=28.4546
+my_long=117.9436
+response=requests.get(url=f"https://api.openweathermap.org/data/2.5/forecast?lat={my_lat}&lon={my_long}&appid={api_key}&cnt=4")
+response.raise_for_status()
+weather_data=response.json()
+for d in weather_data["list"]:
+    for i in d["weather"]:
+        if i["id"]<700:
+            raining=True
+if raining==True:
+    client=Client(sid,token)
+    message=client.messages \
+    .create(body="Чакаецца дождж у бліжэйшыя гадзіны. Мо і ракетны сыпане.",from_="whatsapp:+14155238886",to="whatsapp:+972533714240")
+    print(message.status)
 
-from datetime import datetime
-import pandas
-import random
-import smtplib
-import os
-
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
-
-today = datetime.now()
-today_tuple = (today.month, today.day)
-
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
-
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
